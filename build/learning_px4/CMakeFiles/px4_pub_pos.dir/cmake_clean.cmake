f177e4656d7a53032387c6ef8243dc29cefdbbc7
ChenZehao-rgb <1784359802@qq.com>
2024-07-17 22:11:47 +0800

    chore: Update progress.marks files

---
+++ build/learning_px4/CMakeFiles/px4_pub_pos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/chen/my_px4_workspace/devel/lib/learning_px4/px4_pub_pos"
+  "/home/chen/my_px4_workspace/devel/lib/learning_px4/px4_pub_pos.pdb"
+  "CMakeFiles/px4_pub_pos.dir/src/get_and_pub.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/px4_pub_pos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
